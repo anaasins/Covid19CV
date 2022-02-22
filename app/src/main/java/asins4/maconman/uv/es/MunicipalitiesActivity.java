321b1677293126b18a87f0e6a63baedeaef43e3b
anaasins <asinsanna@gmail.com>
2022-02-22 14:54:37 +0100

    First commit

---
+++ app/src/main/java/asins4/maconman/uv/es/MunicipalitiesActivity.java
@@ -0,0 +1,14 @@
+package asins4.maconman.uv.es;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MunicipalitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_municipalities);
+    }
+}
